215ffa90b6794a9e3c33994777dd8f1d19f10cd5
Jack Letourneau <jletourneau@agoragames.com>
2013-12-09 12:22:24 -0500

    Tweak configs to fix build errors.

--- config.rb
+++ config.rb
@@ -4,20 +4,23 @@ set :images_dir, 'assets/images'
 set :fonts_dir, 'assets/fonts'
 
 set :layouts_dir, 'templates/layouts'
+ignore "templates/layouts/*"
+
 set :partials_dir, 'templates/partials'
+ignore "templates/partials/*"
 
 activate :asset_hash
 activate :automatic_image_sizes
 
 activate :blog do |blog|
   blog.prefix = "blog"
-  blog.permalink = ":year-:month-:day-:title"
+  blog.permalink = ":year-:month-:day-:title.html"
   blog.layout = "blog"
   blog.default_extension = ".markdown"
 
-  blog.year_link = "{year}"
-  blog.month_link = "{year}/{month}"
-  blog.day_link = "{year}/{month}/{day}"
+  blog.year_link = "{year}.html"
+  blog.month_link = "{year}/{month}.html"
+  blog.day_link = "{year}/{month}/{day}.html"
   blog.calendar_template = "blog/calendar.html"
 end
 
