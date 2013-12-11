49d3856ec619a0ce82e4df84a85a2788d5f9ceff
David Czarnecki <dczarnecki@agoragames.com>
2013-12-11 13:53:43 -0500

    Site updated to 49f4823

---
+++ assets/javascripts/plugins/jquery.slideFade-63000bf0.js
@@ -0,0 +1,1 @@
+jQuery.fn.slideFadeIn=function(e,i,t){return this.animate({opacity:"1.0",height:"show"},e,i,t)},jQuery.fn.slideFadeOut=function(e,i,t){return this.animate({opacity:0,height:"hide"},e,i,t)};
