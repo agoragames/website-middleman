37a5f3f17b594a2f4fb8c653ac4a6f2133ced6b1
Jenkins CI <jenkins@ny2-do-agora-ci.tesla>
2015-03-16 17:02:10 +0000

    Site updated to 82729b8

--- assets/javascripts/plugins/jquery.equalHeights-1.0-b3682a8b.js
+++
@@ -1,1 +0,0 @@
-!function(e){e.fn.equalHeights=function(t,n){return tallest=t?t:0,this.each(function(){e(this).height()>tallest&&(tallest=e(this).height())}),n&&tallest>n&&(tallest=n),this.each(function(){e(this).height(tallest).css("overflow","auto")})}}(jQuery);
